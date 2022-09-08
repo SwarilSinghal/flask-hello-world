e4106389157a4537c8061d7067460f97dcdde97f
Swaril Singhal <swaril.singhal@saavn.com>
2022-09-08 15:35:34 +0530

    indent fix

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_session import Session
 app = Flask(__name__)
 app.secret_key = "testing"
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 
 @app.route('/')
 def menu():
